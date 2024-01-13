4b8d0edaee772a29b4fb9c99bd6171d2dd54d44b
devlrakib <devrakib5@gmail.com>
2024-01-14 03:01:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var number = Number(prompt("Enter A Number"));
+// if(number>0){
+//     console.log("Positive");
+// }
+// else{
+//     console.log("Negative");
+// }
+
+
+// Ternary Operator
+result =number>0 ? "Positive":number<0 ? "Negative":"Zero";
+console.log(result);
